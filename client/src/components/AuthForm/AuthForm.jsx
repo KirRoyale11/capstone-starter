4e029c8802c33248ea201af6140ab2b2be70a085
Kirsti Esch <miss.k.esch@gmail.com>
2024-09-19 12:36:38 -0400

    register form variant

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -1,27 +1,48 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-const AuthForm = ({ authAction, mode='login' })=> {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+const AuthForm = ({ authAction, mode = "login" }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const submit = async(ev) => {
+  const submit = async (ev) => {
     ev.preventDefault();
     try {
       await authAction({ username, password }, mode);
-    }
-    catch(ex){
+    } catch (ex) {
       setError(ex.error);
     }
-  }
+  };
   return (
-    <form onSubmit={ submit }>
-      { !!error && <div className='error'>{ error }</div> }
-      <input value={ username } placeholder='username' onChange={ ev=> setUsername(ev.target.value)}/>
-      <input value={ password} placeholder='password' onChange={ ev=> setPassword(ev.target.value)}/>
-      <button>{ mode }</button>
+    <form onSubmit={submit}>
+      {!!error && <div className="error">{error}</div>}
+      {mode === "register" && (
+        <>
+          <input
+            value={firstname}
+            placeholder="First Name"
+            onChange={(ev) => setFirstName(ev.target.value)}
+          />
+          <input
+            value={lastname}
+            placeholder="Last Name"
+            onChange={(ev) => setLastName(ev.target.value)}
+          />
+        </>
+      )}
+      <input
+        value={username}
+        placeholder="Username"
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        value={password}
+        placeholder="Password"
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>{mode}</button>
     </form>
   );
-}
+};
 
 export default AuthForm;
